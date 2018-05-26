1cf249011f8a8b9aa3fcd1babce30dd0e43d9887
16110303 <sepralis.n@students.amikom.ac.id>
2018-05-26 11:58:23 +0700

    kuis

---
+++ kuis/index.php/index.header.php
@@ -0,0 +1,11 @@
+<header>
+    <h1 class="title-site">Belajar Bootstrap</h1>
+    <p class="description">Belajar Membuat Template Website Dengan Bootstrap</p>
+</header>
+<nav class="menus">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Portfolio</a></li>
+    </ul>
+</nav>
